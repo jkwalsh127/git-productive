882de75199acfe23a468ff0eb72d29d00edf2b21
Jake Walsh <jkwalsh127@gmail.com>
2022-05-10 12:09:14 -0700

    initiate project page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,7 +26,7 @@ router.get('/project/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
