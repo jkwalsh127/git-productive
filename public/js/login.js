1a8b138a13f2d5d012a4e7f9791dce96d872db8e
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-10 11:15:07 -0700

    change login frontend js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,22 @@
 //login submission
-async function loginFormHandler(event) {
+const loginFormHandler = async (event) => {
     event.preventDefault();
     // Collect values from the login form
     const username = document.querySelector("#username-login").value.trim();
     const password = document.querySelector("#password-login").value.trim();
-  // Send a POST request to the API endpoint
-    if (username && password) {
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-  // If successful, redirect the browser to the dashboard page
-      if (response.ok) {
-        document.location.replace("/dashboard");
-      } else {
-        alert(response.statusText);
-      }
-    }
+
+  fetch('api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(function() {
+    document.location.replace('profile')
+  })
+  .catch(err => console.log(err))
   }
   
   document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
