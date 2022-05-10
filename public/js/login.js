7d47dcec052d65cbfdcfce43cbbfcabef19044f3
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-10 13:51:46 -0700

    fix login page to direct ot profile page

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,29 @@
 //login submission
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-    // Collect values from the login form
-    const username = document.querySelector("#username-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
-
-  fetch('api/users/login', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: username.value,
-      password: password.value
-    }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then(function() {
-    document.location.replace('/profile')
-  })
-  .catch(err => console.log(err))
+  event.preventDefault();
+  // Collect values from the login form
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  console.log(username)
+  // Send a POST request to the API endpoint
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // If successful, redirect the browser to the dashboard page
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("failed to login");
+    }
   }
-  
-  document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
-  
+};
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
