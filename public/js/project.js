9d700e793181c64c175b96e38e0e8587b4b13525
Jake Walsh <jkwalsh127@gmail.com>
2022-05-11 11:35:14 -0700

    initiate function to save time to db

--- public/js/project.js
+++ public/js/project.js
@@ -5,7 +5,8 @@ const continueBtn = document.getElementById('continue');
 const secondTotal = document.getElementById('second-total');
 const minuteTotal = document.getElementById('minute-total');
 const hourTotal = document.getElementById('hour-total');
-const earnedWage = document.getElementById('earned-wage')
+const earnedWage = document.getElementById('earned-wage');
+const saveTimeBtn = document.getElementById('save-time');
 
 const rate = 100;
 
@@ -54,15 +55,15 @@ const rate = 100;
         sec = 0;
       }
   
-      if (sec < 10 || sec == 0) {
-        sec = '0' + sec;
-      }
-      if (min < 10 || min == 0) {
-        min = '0' + min;
-      }
-      if (hr < 10 || hr == 0) {
-        hr = '0' + hr;
-      }
+      // if (sec < 10 || sec == 0) {
+      //   sec = '0' + sec;
+      // }
+      // if (min < 10 || min == 0) {
+      //   min = '0' + min;
+      // }
+      // if (hr < 10 || hr == 0) {
+      //   hr = '0' + hr;
+      // }
   
       timer.innerHTML = hr + ':' + min + ':' + sec;
 
@@ -91,6 +92,10 @@ function addSeconds(totalSec) {
   if (totalSeconds == 0) {
     return;
   }
+  if (totalSec == 60) {
+    totalMin = totalMin + 1;
+    totalSec = 0;
+  }
   secondTotal.innerHTML = totalSeconds
   return totalSeconds;
 }
@@ -102,6 +107,11 @@ function addMinutes(totalMin) {
   }
   if (totalMinutes == 0) {
     return;
+  }
+  if (totalMin == 60) {
+    totalHr = totalHr + 1;
+    totalMin = 0;
+    totalSec = 0;
   }
   minuteTotal.innerHTML = totalMinutes
   return totalMinutes;
@@ -134,6 +144,10 @@ continueBtn.addEventListener('click', () => {
   resetTimer();
   modalContainer.classList.remove('show');
 });
+continueBtn.addEventListener('click', () => {
+  resetTimer();
+  modalContainer.classList.remove('show');
+});
 
 async function workValue(rate) {
   value = ((rate * totalSeconds) / 3600) + ((rate * totalMinutes) / 60) + (rate * totalHours)
@@ -141,3 +155,40 @@ async function workValue(rate) {
   earnedWage.innerHTML = value;
   return value;
 }
+
+saveTimeBtn.addEventListener('click', () => {
+  // var min = minuteTotal.innerHTML
+  // console.log(min);
+  // var m = (Math.round(min/15) * 15) % 60;
+  // console.log(m);
+  var sec = secondTotal.innerHTML
+  console.log(sec);
+  var s = (Math.round(sec/15) * 15) % 60;
+  console.log(s);
+  
+});
+
+const saveTime = async (event) => {
+  event.preventDefault();
+
+  var sec = secondTotal.innerHTML;
+  var s = (Math.round(sec/15) * 15) % 60;
+  var min = minuteTotal.innerHTML;
+  var m = (Math.round(min/15) * 15) % 60;
+  var hr = hourTotal.innerHTML;
+
+  if (sec && min && hr) {
+    const response = await fetch(`/api/projects`, {
+      method: 'POST',
+      body: JSON.stringify({ sec, min, hr }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      res.render('project');
+    } else {
+      alert('Failed to save time to database');
+    }
+  }
+};
